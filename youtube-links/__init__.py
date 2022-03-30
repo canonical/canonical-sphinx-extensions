f986f87af35a7e85f143773ff8d9d9a662aed01c
Ruth Fuchss <ruth.fuchss@canonical.com>
2022-03-30 09:42:02 +0200

    initial commit of the extensions
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

---
+++ youtube-links/__init__.py
@@ -0,0 +1,35 @@
+from docutils import nodes
+from docutils.parsers.rst import Directive
+from . import common
+
+class YouTubeLink(Directive):
+
+    required_arguments = 1
+    optional_arguments = 0
+    has_content = False
+
+    def run(self):
+
+        fragment = ' \
+        <p class="youtube_link"> \
+          <a href="'+self.arguments[0]+'" target="_blank"> \
+            <span class="play_icon">▶</span> \
+            <span>Watch on YouTube</span> \
+          </a> \
+        </p>'
+        raw = nodes.raw(text=fragment, format="html")
+
+        return [raw]
+
+
+
+def setup(app):
+    app.add_directive("youtube", YouTubeLink)
+
+    common.add_css(app,"youtube.css")
+
+    return {
+        'version': '0.1',
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
