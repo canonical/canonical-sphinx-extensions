765cfc9e8080cc3289d458a25af7d8059966ce49
Ruth Fuchss <ruth.fuchss@canonical.com>
2022-07-20 12:06:13 +0200

    add an extension for custom RST roles
    We need a role to mark up spelling exceptions to remove them
    from the spell checker.
    
    There will probably be more custom roles in the future.
    
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

---
+++ custom-rst-roles/__init__.py
@@ -0,0 +1,12 @@
+from docutils import nodes
+
+def spellexception_role(name, rawtext, text, lineno, inliner,
+            options=None, content=None):
+    node = nodes.raw(text="<spellexception>"+text+"</spellexception>", format="html")
+    return [node],[]
+
+
+def setup(app):
+    app.add_role("spellexception", spellexception_role)
+
+    return
