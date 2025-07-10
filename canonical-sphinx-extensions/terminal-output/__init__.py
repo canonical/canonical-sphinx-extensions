39c52b27f620a7b1fddb6295137a3800251316d8
secondskoll <michael.park@canonical.com>
2025-07-10 10:54:44 +1000

    feat: correct terminal output in PDF

--- canonical-sphinx-extensions/terminal-output/__init__.py
+++ canonical-sphinx-extensions/terminal-output/__init__.py
@@ -1,10 +1,27 @@
 from docutils import nodes
-from docutils.parsers.rst import Directive
+from sphinx.util.docutils import SphinxDirective
 from docutils.parsers.rst import directives
 from . import common
+import sphinx
 
+def parse_contents(contents):
+    command_output = []
+    out = []
 
-class TerminalOutput(Directive):
+    while contents:
+        line = contents[0]
+        contents = contents[1:]
+        if line.startswith(':input: '):
+            out.append(command_output)
+            out.append([line])
+            command_output = []
+        else:
+            command_output.append(line)
+
+    out.append(command_output)
+    return out
+
+class TerminalOutput(SphinxDirective):
 
     required_arguments = 0
     optional_arguments = 0
@@ -23,25 +40,29 @@ class TerminalOutput(Directive):
         inpline = nodes.container()
         inpline["classes"].append("input")
 
+        # To let the prompt be styled separately in LaTeX, it needs to be wrapped in a
+        # container. This adds an extra div to the HTML output, but what's a few bytes
+        # between friends?
+        prompt_container = nodes.container()
+        prompt_container["classes"].append("prompt")
         prompt = nodes.literal(text=prompt_text)
-        prompt["classes"].append("prompt")
-        inpline.append(prompt)
+        prompt_container.append(prompt)
 
+        inpline.append(prompt_container)
         inp = nodes.literal(text=command)
         inp["classes"].append("command")
         inpline.append(inp)
-
+        # inpline.append(nodes.paragraph())
         return inpline
 
     def run(self):
-        # Build prompt with :user:, :host: and :dir: (whichever is provided)
-        user = self.options.get("user")
-        host = self.options.get("host")
-        dir = self.options["dir"] if "dir" in self.options else "~"
-        command = self.options["input"] if "input" in self.options else ""
+        # if :user: or :host: are provided, replace those in the prompt
 
+        command = "" if "input" not in self.options else self.options["input"]
+        user = "user" if "user" not in self.options else self.options["user"]
+        host = "host" if "host" not in self.options else self.options["host"]
+        dir = "~" if "dir" not in self.options else self.options["dir"]
         user_symbol = '#' if user == "root" else '$'
-
         if user and host:
             prompt_text = f"{user}@{host}:{dir}{user_symbol} "
         elif user and not host:
@@ -54,28 +75,35 @@ class TerminalOutput(Directive):
 
         out = nodes.container()
         out["classes"].append("terminal")
+        # The super-large value for linenothreshold is a major hack since I can't
+        # figure out how to disable line numbering and the linenothreshold kwarg seems
+        # to be required.
+        out.append(sphinx.addnodes.highlightlang(lang="text", force=False, linenothreshold=10000))
         if "scroll" in self.options:
             out["classes"].append("scroll")
 
-        # Add the original prompt and input if the command is present
+        # Add the original prompt and input
 
-        if command:
-            out.append(self.input_line(prompt_text, command))
-
+        out.append(self.input_line(prompt_text, command))
+        # breakpoint()
+        only_input = all((line.startswith(":input: ") for line in self.content))
+        if only_input:
+            out.append(nodes.paragraph())
         # Go through the content and append all lines as output
         # except for the ones that start with ":input: " - those get
         # a prompt
 
-        for output in self.content:
-            if output.startswith(":input: "):
-                out.append(self.input_line(prompt_text, output[8:]))
-            else:
-                if output == "":
-                    output = " "
-                outp = nodes.literal(text=output)
-                outp["classes"].append("output")
-                out.append(outp)
+        parsed_content = parse_contents(self.content)
 
+        for blob in parsed_content:
+            if not blob:
+                continue
+            if blob[0].startswith(':input: '):
+                out.append(self.input_line(prompt_text, blob[0][8:]))
+            else:
+                out.append(nodes.literal_block(text='\n'.join(blob)))
+            if only_input:
+                out.append(nodes.paragraph())
         return [out]
 
 
